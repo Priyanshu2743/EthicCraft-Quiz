19ce7e5f4edb6bcb27de374f3333ba493a672fe7
geekymca <kumarpankaj12830@gmail.com>
2023-12-20 21:14:44 +0530

    "updated app.js"

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const http = require("http");
 const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -77,8 +78,8 @@ app.get('/', function (req, res) {
 })
 
 // Start the server
-app.listen(3000, () => {
-    console.log('Server started on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
 
 
